9f09351ed526080e5d0bc6a30346f49de9837999
Mikhail <mishnade@gmail.com>
2016-05-06 12:00:57 -0700

    writing some tests first

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,3 +2,20 @@ import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
 
 chai.use(chaiImmutable);
+
+
+describe('immutability', ()=> {
+  describe('a number', ()=> {
+    function increment(currentState) {
+      return currentState + 1;
+    }
+
+    it('is immutable', ()=> {
+      let state = 42;
+      let nextState = increment(state);
+
+      expect(nextState).to.equal.(43);
+      expect(state).to.equal(42);
+    });
+  });
+});
